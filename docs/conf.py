24a58250e7b8afaa9fd540234d7bbe827b1851f7
Stefan Meinecke <meinecke@greensec.de>
2026-09-17 11:36:16 +0200

    Add Stefan Meinecke to docs copyright

--- docs/conf.py
+++ docs/conf.py
@@ -46,7 +46,7 @@ master_doc = "index"
 
 # General information about the project.
 project = "PySeaweed"
-copyright = "2014-2026, Łukasz Bołdys"
+copyright = "2014-2026, Łukasz Bołdys, Stefan Meinecke"
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
